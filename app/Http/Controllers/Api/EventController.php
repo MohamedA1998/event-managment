d97ad6caab369238c919569b93e47ecc64e86167
Mohamed Alsayed <malsayed493@gmail.com>
2025-10-24 01:10:57 +0300

    Loading Relation In event controller

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -15,7 +15,31 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::with('user')->paginate(10)->toResourceCollection();
+        $query = Event::query();
+
+        $relations = ['user', 'attendees', 'attendees.user'];
+
+        foreach ($relations as $relation) {
+            $query->when(
+                $this->shouldIncludeRelation($relation),
+                fn($q) => $q->with($relation)
+            );
+        }
+
+        return $query->paginate(10)->toResourceCollection();
+    }
+
+    protected function shouldIncludeRelation(string $relation): bool
+    {
+        $include = request()->query('include');
+
+        if (!$include) {
+            return false;
+        }
+
+        $relations = array_map('trim', explode(',', $include));
+
+        return in_array($relation, $relations);
     }
 
     /**
