0b2b990e09a5db7983bc90789d2d4ea574928ff0
Mohamed Alsayed <malsayed493@gmail.com>
2025-10-23 17:48:49 +0300

    Handel event Controller

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Attendee;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class AttendeeController extends Controller
@@ -11,40 +12,38 @@ class AttendeeController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Event $event)
     {
-        //
+        return $event->attendees()->latest()->paginate(10)->toResourceCollection();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request, Event $event)
     {
-        //
+        $attendee = $event->attendees()->create([
+            'user_id' => 1
+        ]);
+
+        return $attendee->toResource();
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Attendee $attendee)
+    public function show(Event $event, Attendee $attendee)
     {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Attendee $attendee)
-    {
-        //
+        return $attendee->toResource();
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Attendee $attendee)
+    public function destroy(Event $event, Attendee $attendee)
     {
-        //
+        $attendee->delete();
+
+        return response(status: 204);
     }
 }
