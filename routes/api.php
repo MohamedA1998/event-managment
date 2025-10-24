841fe300b2480e747782f1239c693011c0742e13
Mohamed Alsayed <malsayed493@gmail.com>
2025-10-24 12:05:13 +0300

    Handel route login and logout

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\{EventController, AttendeeController};
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:api');
 
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
     ->scoped()->except('update');
+
+Route::resource('auth', AuthController::class)
+    ->only('store', 'destroy');
